3fbfd0c8da531eee94a3540dc7502b7b6f36124f
Christopher Alberto Murillo Castro <97977019+SMIS903520@users.noreply.github.com>
2022-02-26 16:01:32 -0600

    Agregar componente de simbolos patrios

---
+++ appweb/src/components/simbolosPatrios.vue
@@ -0,0 +1,79 @@
+<template>
+<div class="simbolos">
+   <font color="171D1C" face="Open Sans"><h1><em>Simbolos Patrios de El Salvador</em></h1></font>
+    <div class="container px-5">
+     <div class="row gx-5">
+       <div class="col-lg-8 align-center">
+         <b-card-group deck>
+            <b-card title="Bandera Nacional" img-src="https://www.elsalvadormipais.com/wp-content/uploads/2017/01/bandera-de-el-salvador.png" img-alt="Image" img-top>
+              <b-card-text>
+                <h3> bandera nacional de El Salvador está compuesta por tres franjas horizontales de igual tamaño; 
+                la de arriba y la de abajo son de color azul cobalto y la del centro es blanca.</h3>
+              </b-card-text>
+            </b-card>
+              <br>
+              <br>
+             <div>
+               <b-card title="Escudo Nacional" img-src="https://www.elsalvadormipais.com/wp-content/uploads/2017/01/escudo-de-el-salvador-1.png" img-alt="Image" img-top>
+                   <b-card-text>
+                     <h3>El escudo nacional de El Salvador es el emblema nacional que representa al país, 
+                     tanto dentro del territorio nacional como fuera de nuestras fronteras patrias.</h3>
+                   </b-card-text>
+                </b-card>
+                <br>
+                <br>
+              </div>
+               <div>
+                 <b-card title="Ave Nacional" img-src="https://www.elsalvadormipais.com/wp-content/uploads/2017/01/torogoz-ave-nacional-el-salvador.jpg" img-alt="Image" img-top>
+                   <b-card-text>
+                     <h3>El torogoz o talapo es el ave nacional de El Salvador, fue declarada como tal por un Acuerdo Legislativo que se aprobó el día 21 de octubre de 1999.</h3>
+                   </b-card-text>
+                  </b-card>
+                  <br>
+                  <br>
+               </div>
+                 <div>
+                   <b-card title="Arbol Nacional" img-src="https://www.elsalvadormipais.com/wp-content/uploads/2017/01/maquilishuat-arbol-nacional-de-el-salvador.jpg" img-alt="Image" img-top>
+                    <b-card-text>
+                       <h3>El maquilishuat es un árbol muy hermoso, ya que en la época donde se caen sus hojas es cubierto totalmente por unas flores rosadas.
+                       Este árbol puede medir hasta 15 metros de altura. Sus grandes y majestuosas flores de color rosa, ofrecen distintos grados de intensidad en su coloración.
+                        Cuando el maquilishuat florece, es uno de los más bellos de la región.</h3>
+                    </b-card-text>
+                   </b-card>
+                   <br>
+                   <br>
+                 </div>
+                 <div>
+                    <b-card title="Hinno Nacional" img-src="https://www.elsalvadormipais.com/wp-content/uploads/2017/09/himno-nacional-el-salvador.jpg" img-alt="Image" img-top>
+                      <b-card-text>
+                        <h3>El Himno Nacional de El Salvador fue compuesto por el general salvadoreño Juan José Cañas y el músico italiano Juan Aberle.
+                        Se usó por primera vez el 15 de septiembre de 1879, pero fue adoptado oficialmente hasta el 13 de noviembre de 1953.</h3>
+                      </b-card-text>
+                    </b-card>
+                    <br>
+                    <br>
+                 </div>
+                   <div>
+                     <b-card title="Comida Tipica" img-src="https://www.elsalvadormipais.com/wp-content/uploads/2017/01/dia-nacional-de-las-pupusas.jpg" img-alt="Image" img-top>
+                        <b-card-text>
+                          <h3>Como un símbolo patrio nacional, pero si como parte de nuestra identidad, 
+                          hemos decidido colocar en este espacio la comida típica nacional del país, la cual es las pupusas
+                          Es común encontrar pupusas en cualquier lugar de el Salvador, de hecho no hay salvadoreño que no las haya probado.
+                          Se pueden encontrar de varios ingredientes; de masa, de arroz, de queso, de frijol, de chicharrón, de mora, de ayote, de loroco, etc.</h3>
+                        </b-card-text>
+                     </b-card>
+                  </div>
+          </b-card-group>
+         </div>
+      </div>
+   </div>
+</div>
+
+</template>
+
+<script>
+export default {
+    name: 'simbolos',
+
+}
+</script>
