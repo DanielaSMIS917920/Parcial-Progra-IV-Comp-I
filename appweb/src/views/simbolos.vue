88285f37e399ca9c969b7b9ea686e7b77986db68
Christopher Alberto Murillo Castro <97977019+SMIS903520@users.noreply.github.com>
2022-02-26 16:02:09 -0600

    Agregar vista de simbolos patrios

---
+++ appweb/src/views/simbolos.vue
@@ -0,0 +1,17 @@
+<template>
+ <div>
+     <h1>Conozca los simbolos patrios </h1>
+     <simbolosPatrios/>
+ </div>
+</template>
+
+<script>
+import simbolosPatrios from '@/components/simbolosPatrios.vue'
+
+export default {
+    name: 'simbolos',
+    components: { 
+        simbolosPatrios 
+        }
+}
+</script>
