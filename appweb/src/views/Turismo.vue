21402b4f2b64e6099e538d221c6fd76a312a5f9b
Hayci Natalia Zuniga Ramirez <97977137+SMIS947720@users.noreply.github.com>
2022-02-26 23:51:56 -0600

    views Turismo

---
+++ appweb/src/views/Turismo.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="app">
+    <Viajes title="Lugares Turisticos"/>
+    
+    </div>
+</template>
+
+
+<script>
+
+//Import de componentes
+import Viajes from '@/components/Viajes.vue'
+
+
+export default {
+    name: 'Turismo',
+    components:{
+        Viajes
+    }
+    
+}
+</script>
